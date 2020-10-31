cc1cb49848023aeacc5500827d91b6bd3a86f765
chfigure <2553109663@qq.com>
2020-11-01 05:13:32 +0800

    完善流水、余额统计前端（加分割线），完成数据库升级

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/billSortByYear.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class billSortByYear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_sort_by_year);
+    }
+}
